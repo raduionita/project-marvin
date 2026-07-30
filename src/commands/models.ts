ad30d6b0b757c0002f863f99411bb6f4248cff62
Radu Ionita <ionita.radu.ionut@gmail.com>
2026-07-30 14:32:45 +0300

    0.1.18

--- src/commands/models.ts
+++ src/commands/models.ts
@@ -1,4 +1,4 @@
-import readline from 'readline';
+import readline, { promises } from 'readline';
 
 import { listModels } from "../models";
 import { Command, Config, Provider } from "../types";
@@ -38,50 +38,30 @@ export default class ModelsCommand extends Command {
       } break;
       case 'add' : {
         const config = {} as Config['models'][string];
-        // ask for provder
-        const rl = readline.createInterface({input: process.stdin, output: process.stdout});
-        config['provider'] = await new Promise<Provider>((resolve) => {
-          rl.question('Enter provider [openai, anthropic, deepseek, lmstudio]: ', (ans: string) => {
-            resolve(ans as Provider);
-            rl.close();
-          });
-        });
-        // ask for model
-        config['model'] = await new Promise<string>((resolve) => {
-          rl.question('Enter model (e.g. gpt-3.5-turbo): ', (ans: string) => {
-            resolve(ans);
-            rl.close();
-          });
-        });
-        // ask for baseUrl or empty/default
-        config['baseUrl'] = await new Promise<string>((resolve) => {
-          rl.question('Enter baseUrl (e.g. http://localhost:1234): ', (ans: string) => {
-            resolve(ans);
-            rl.close();
-          });
-        });
+        
+        console.log('');
+        const pli = promises.createInterface({input: process.stdin, output: process.stdout, });
+        config['provider'] = await pli.question('Enter provider [openai, anthropic, deepseek, lmstudio]: ') as Provider;
+        config['model']    = await pli.question('Enter model name (e.g. gpt-3.5-turbo): ');
+        config['baseUrl']  = await pli.question('Enter baseUrl (e.g. http://localhost:1234): ');
+        config['apiKey']   = await pli.question('Enter apiKey (e.g. sk-1234): ');
+        pli.close();
+        console.log('');
+        
         if (!config['baseUrl']) delete config['baseUrl'];
-        // ask for apiKey or empty/default
-        config['apiKey'] = await new Promise<string>((resolve) => {
-          rl.question('Enter apiKey (e.g. sk-1234): ', (ans: string) => {
-            resolve(ans);
-            rl.close();
-          });
-        });
-
         config['enabled'] = true;
 
-        const id = config['provider'] + '/' + config['model'];
+        const modelId = config['provider'] + '/' + config['model'];
 
-        this.ctx.config.models[id] = config;
+        this.ctx.config.models[modelId] = config;
 
         const cpath = join(this.ctx.home, 'marvin.json');
         // write to config file
         if (this.ctx.isDry) {
-          console.info('[ModelsCommand.exec]', '[dry]',`would configure model ${id}, config persisted to ${cpath}`);
+          console.info('[ModelsCommand.exec]', '[dry]',`would configure model "${modelId}", config persisted to ${cpath}`);
         } else {
           writeFileSync(cpath, JSON.stringify(this.ctx.config, null, 2));
-          console.info('[ModelsCommand.exec]', `model "${id}" configured, config persisted to ${cpath}`);
+          console.info('[ModelsCommand.exec]', `model "${modelId}" configured, config persisted to ${cpath}`);
         }
       } break;
     }
